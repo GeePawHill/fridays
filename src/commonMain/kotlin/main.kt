e51eb4040d31cc5f8428f06253c5c1757738e9c4
Bryan Beecham <bryan_beecham@hotmail.com>
2022-07-29 09:17:45 -0400

    Changed scale just to make a change

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -15,7 +15,7 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 	val image = image(resourcesVfs["korge.png"].readBitmap()) {
 		rotation = maxDegrees
 		anchor(.5, .5)
-		scale(.8)
+		scale(.9)
 		position(256, 256)
 	}
 
