72da51b80fe4cc23bfdedcadb0b4d15a40904267
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-28 23:58:16 -0400

    The korge generated app.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,36 @@
-plugins {
-    kotlin("jvm") version "1.6.21"
+import com.soywiz.korge.gradle.*
+
+buildscript {
+	val korgePluginVersion: String by project
+
+	repositories {
+		mavenLocal()
+		mavenCentral()
+		google()
+		maven { url = uri("https://plugins.gradle.org/m2/") }
+	}
+	dependencies {
+		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+	}
 }
 
-repositories {
-    mavenCentral()
-}
+apply<KorgeGradlePlugin>()
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+korge {
+	id = "com.example.example"
+// To enable all targets at once
+
+	//targetAll()
+
+// To enable targets based on properties/environment variables
+	//targetDefault()
+
+// To selectively enable targets
+
+	targetJvm()
+	targetJs()
+	targetDesktop()
+	targetIos()
+	targetAndroidIndirect()
+// targetAndroidDirect()
 }
